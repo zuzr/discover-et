139460369ff548cffc81822cf9933bbb0fff49f6
Ryan Zuzelski <rzuzelski@gmail.com>
2019-03-04 22:15:57 -0800

    sontent placeholders

--- src/App.js
+++ src/App.js
@@ -10,48 +10,120 @@ class App extends Component {
           <header>
             <nav class="navbar navbar-default navbar-static-top">
               <container class="container-fluid">
-                <a class="navbar-brand navbar-left" href="/">Logo</a>
-                <ul class="nav navbar-nav navbar-left row">
-                  <li><a class="navbar-item" href="#page1">Home</a></li>
-                  <li><a class="navbar-item" href="#page2">Mercury</a></li>
-                  <li><a class="navbar-item" href="#page3">Venus</a></li>
-                  <li><a class="navbar-item" href="#page4">Earth</a></li>
+                <ul class="nav navbar-nav">
+                  <li><a class="navbar-item" href="#Home">Home</a></li>
+                  <li><a class="navbar-item" href="#Mercury">Mercury</a></li>
+                  <li><a class="navbar-item" href="#Venus">Venus</a></li>
+                  <li><a class="navbar-item" href="#page3">Earth</a></li>
+                  <li><a class="navbar-item" href="#page4">Mars</a></li>
+                  <li><a class="navbar-item" href="#page4">Asteroid Belt</a></li>
+                  <li><a class="navbar-item" href="#page4">Jupiter</a></li>
+                  <li><a class="navbar-item" href="#page4">Saturn</a></li>
+                  <li><a class="navbar-item" href="#page2">Uranus</a></li>
+                  <li><a class="navbar-item" href="#page2">Neptune</a></li>
+                  <li><a class="navbar-item" href="#page2">Outer Orbit</a></li>
                 </ul>
               </container>
             </nav>
           </header>
 
           <body>
-            <div id="page1" class="page">
-              <a id="Home"></a>
+            <page id="page1" class="page">
+              <a id="Sun"></a>
               <content class="content">
                 <p class="blurb text-center">Welcome to discoverET</p>
               </content>
-            </div>
+            </page>
             
-            <div id="page2" class="page">
+            <page id="page2" class="page">
               <a id="Mercury"></a>
               <content class="content">
-                <p className="blurb">13.82 years ago, the universe was born.
+
+                <p className="blurb text-center">(Reasons why Mercury is not a target for ET search)
                 </p>
               </content>
-            </div>
+            </page>
             
-            <div id="page3" class="page">
+            <page id="page3" class="page">
               <a id="Venus"></a>
-              <content class="content"> 
-                <p class="blurb">All matter that exists in our universe today expanded from an infinitely small and dense singularity into the endless expanse of the cosmos.
+              <content class="content">
+                
+                <p class="blurb text-center">Venus' high Deuterium-Hydrogen ratio hints that the planet may have had liquid water in the past.
                 </p>
               </content> 
-            </div>
+            </page>
 
-            <div id="page4" class="page">
+            <page id="page4" class="page">
               <a id="Earth"></a>
               <content class="content">
-                <p class="blurb">Earth formed from accumulation of matter in the orbit of the sun.
+                
+                <p class="blurb text-center">Extremophiles discovered on Earth refine our idea of habitable conditons.
                 </p>
               </content>
-            </div>
+            </page>
+
+            <page id="page5" class="page">
+              <a id="Mars"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">Mars' polar ice caps are currently intact and suggest liquid water may have existed or still does exist on the planet.
+                </p>
+              </content>
+            </page>
+
+            <page id="page6" class="page">
+              <a id="Asteroid-Belt"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">The Asteroid belt hosts many large planetoids that may be habitable. Ceres is one of these bodies and has its own atmosphere, primarily composed of water vapor.
+                </p>
+              </content>
+            </page>
+
+            <page id="page7" class="page">
+              <a id="Jupiter"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">While Jupiter itself is not a target for ET search, some of its large moons are of interest. Geothermal activity has been detected on each, and Ganymede and Callisto are likely to have subsurface liquid water oceans.
+                </p>
+              </content>
+            </page>
+
+            <page id="page8" class="page">
+              <a id="Saturn"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">Saturn is like Jupiter, in that we are not confident in its ability to host ET life. However, its largest moon, Titan, has an atmosphere and conditions that could potentially support life.
+                </p>
+              </content>
+            </page>
+
+            <page id="page9" class="page">
+              <a id="Uranus"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">(Reasons why Uranus is not a target for ET search)
+                </p>
+              </content>
+            </page>
+
+            <page id="page10" class="page">
+              <a id="Neptune"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">(Reasons why Neptune is not a target for ET search)
+                </p>
+              </content>
+            </page>
+
+            <page id="page11" class="page">
+              <a id="Outer-Orbit"></a>
+              <content class="content">
+                
+                <p class="blurb text-center">(Reasons why outer-orbit planetoids are not targets for ET search)
+                </p>
+              </content>
+            </page>
           </body>
         </main>
         
